33246f620edbfd6d6ac78494dbb8273dd75a99ac
Marin Atanasov <tyxla@abv.bg>
2014-11-22 20:44:09 +0200

    adding some comments

--- LiveReload/Program.cs
+++ LiveReload/Program.cs
@@ -6,16 +6,19 @@ using System.Windows.Forms;
 
 namespace LiveReload
 {
+    // Main application class
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
+            // Initialize our visual styles
             Application.EnableVisualStyles();
+            
+            // Setup text rendering compatibility
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Initialize and run the main LiveReload instance
             Application.Run(new LiveReload());
         }
     }
