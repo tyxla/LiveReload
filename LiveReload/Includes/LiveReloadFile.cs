2edce6628dae14fa9e005108f85fe8ad0b2cb936
Marin Atanasov <tyxla@abv.bg>
2014-11-22 23:16:58 +0200

    adding comments on LiveReloadFile

--- LiveReload/Includes/LiveReloadFile.cs
+++ LiveReload/Includes/LiveReloadFile.cs
@@ -7,17 +7,23 @@ using System.IO;
 
 namespace LiveReload
 {
+    // Abstract class for a file that is being saved.
     abstract class LiveReloadFile
     {
+        // Constructor. Specifies the path of the updated file
         public LiveReloadFile(string filePath)
         {
             this.FilePath = filePath;
         }
 
+        // Process the file after being saved.
+        // Must be implemented in the child classes.
         abstract public void Process();
 
+        // Contains the file path
         private string filePath;
 
+        // Setter & getter for file path
         protected string FilePath
         {
             get { return filePath; }
