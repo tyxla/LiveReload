e6649f7d4c9a9d31f530b20e07e980b264a841db
Marin Atanasov <tyxla@abv.bg>
2014-12-14 17:43:53 +0200

    LiveReloadBrowser, implement refresh of all common open browsers on tracked HTML file save

--- LiveReload/Includes/LiveReloadBrowser.cs
+++ LiveReload/Includes/LiveReloadBrowser.cs
@@ -15,38 +15,43 @@ namespace LiveReload
 
         public static void Refresh() {
 
-            // Refresh Chrome
-            Process[] procsChrome = Process.GetProcessesByName("chrome");
-            foreach (Process chrome in procsChrome)
+            // Define all browsers that we will refresh
+            string[] browsers = {"chrome", "firefox", "opera", "iexplore", "safari"};
+
+            // Refresh all browsers
+            foreach (string browser in browsers)
             {
-                if (chrome.MainWindowHandle != IntPtr.Zero)
+                Process[] browserProcesses = Process.GetProcessesByName(browser);
+                foreach (Process browserProcess in browserProcesses)
                 {
-                    // Save the currently focused window
-                    IntPtr currentWindowHandle = GetForegroundWindow();
+                    if (browserProcess.MainWindowHandle != IntPtr.Zero)
+                    {
+                        // Save the currently focused window
+                        IntPtr currentWindowHandle = GetForegroundWindow();
 
-                    // Set focus on the window so that the key input can be received.
-                    LiveReloadSendInput.SetForegroundWindow(chrome.MainWindowHandle);
+                        // Set focus on the window so that the key input can be received.
+                        LiveReloadSendInput.SetForegroundWindow(browserProcess.MainWindowHandle);
 
-                    // Create a F5 key press
-                    LiveReloadSendInput.INPUT ip = new LiveReloadSendInput.INPUT { Type = 1 };
-                    ip.Data.Keyboard = new LiveReloadSendInput.KEYBDINPUT();
-                    ip.Data.Keyboard.Vk = (ushort)0x74;  // F5 Key
-                    ip.Data.Keyboard.Scan = 0;
-                    ip.Data.Keyboard.Flags = 0;
-                    ip.Data.Keyboard.Time = 0;
-                    ip.Data.Keyboard.ExtraInfo = IntPtr.Zero;
+                        // Create a F5 key press
+                        LiveReloadSendInput.INPUT ip = new LiveReloadSendInput.INPUT { Type = 1 };
+                        ip.Data.Keyboard = new LiveReloadSendInput.KEYBDINPUT();
+                        ip.Data.Keyboard.Vk = (ushort)0x74;  // F5 Key
+                        ip.Data.Keyboard.Scan = 0;
+                        ip.Data.Keyboard.Flags = 0;
+                        ip.Data.Keyboard.Time = 0;
+                        ip.Data.Keyboard.ExtraInfo = IntPtr.Zero;
 
-                    var inputs = new LiveReloadSendInput.INPUT[] { ip };
+                        var inputs = new LiveReloadSendInput.INPUT[] { ip };
 
-                    // Send the keypress to the window
-                    LiveReloadSendInput.SendInput(1, inputs, Marshal.SizeOf(typeof(LiveReloadSendInput.INPUT)));
+                        // Send the keypress to the window
+                        LiveReloadSendInput.SendInput(1, inputs, Marshal.SizeOf(typeof(LiveReloadSendInput.INPUT)));
 
-                    // Set focus back 
-                    LiveReloadSendInput.SetForegroundWindow(currentWindowHandle);
+                        // Set focus back 
+                        LiveReloadSendInput.SetForegroundWindow(currentWindowHandle);
+                    }
                 }
             }
 
-            // TODO: refresh other famous browsers
         }
     }
 }
